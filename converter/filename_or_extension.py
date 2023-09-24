cf92ed5a75451450c0ae9bb9bee277dbfa323493
evgeniy <pavlinbl4@gmail.com>
2023-09-24 15:35:49 +0300

    add new regex to cut off file extension

--- converter/filename_or_extension.py
+++ converter/filename_or_extension.py
@@ -5,6 +5,10 @@ def select_file_name(file_name):
     pattern = r'.+(?=\.)'
     return re.search(pattern, file_name).group()
 
+def cut_off_extension_from_filename(file_name):
+    pattern = r'.\w+$'
+    return re.sub(pattern,'', file_name)
+
 
 def select_file_extension(file_name):
     pattern = r'(?<=\.).+'
@@ -13,3 +17,9 @@ def select_file_extension(file_name):
 
 def list_string_way(file_name):
     return file_name.split('.')
+
+
+if __name__ == '__main__':
+    print(select_file_name(
+        'Эдгар Уоллес. Тайна булавки. Главы 1-3..mp4'))
+    print(cut_off_extension_from_filename('Эдгар Уоллес. Тайна булавки. Главы 1-3..mp4'))
