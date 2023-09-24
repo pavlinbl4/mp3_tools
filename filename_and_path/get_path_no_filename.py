82e375e9dc2bab13583d1dd3d66f2dc9a76d2bd1
evgeniy <pavlinbl4@gmail.com>
2023-09-24 17:11:22 +0300

    3 ways to get directory path

---
+++ filename_and_path/get_path_no_filename.py
@@ -0,0 +1,17 @@
+#  a few ways to get the directory path of a file without the filename in Python:
+
+import os
+from pathlib import Path
+
+def der_path(full_path):
+    # using os
+    dir_path = os.path.dirname(full_path)
+
+    # Using pathlib.Path.parent
+    dir_path = Path(full_path).parent
+
+    # Splitting the string on slash and joining back
+    dir_path = '/'.join(full_path.split('/')[:-1])
+
+
+
