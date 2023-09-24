bb9321d03038ef35d15272821ec950c671ef796a
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-09-24 20:57:51 +0300

    select folder with files via gui

---
+++ tools/tk_tools.py
@@ -0,0 +1,21 @@
+from tkinter import filedialog, messagebox, Tk
+
+
+
+def select_folder_via_gui(initialdir):
+    root = Tk()
+    root.withdraw()
+
+    choose_folder = filedialog.askdirectory(
+        initialdir=initialdir,
+        title="Select your Source directory")
+    if len(choose_folder) > 0:
+        return choose_folder
+    else:
+        messagebox.showwarning("Warning", "You haven't chosen a folder. Program terminated.")
+        root.destroy()
+
+
+if __name__ == '__main__':
+    # display_info("NO KEYWORDS FOR WORK")
+    select_folder_via_gui('/Volumes/big4photo-4/EDITED_JPEG_ARCHIV/Downloaded_from_fotoagency')
